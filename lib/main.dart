46d1f9a4420a48f81d22709f08a21f264fb84fe1
“Nobuhiro <nobuhiro.masaka@myrefer.co.jp>
2023-10-09 15:18:10 +0900

    slack通知試す

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo22',
+      title: 'Flutter Demo3',
       theme: ThemeData(
         // This is the theme of your application.
         //
